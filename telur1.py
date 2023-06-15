0f9eb9be973e16c5effdbcd27ebcfda5e1720a63
Anggi Novitasari <117124282+Ngno@users.noreply.github.com>
2023-06-15 09:53:24 +0700

    Update telur1.py
    add 1 chart

--- telur1.py
+++ telur1.py
@@ -2,6 +2,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import plotly.express as px
 
 def main():
     # Set page configuration
@@ -20,15 +21,14 @@ def main():
     if menu_selection == "Data Story":
       #  st.header("Chapter I: Data Pengantar")
         st.subheader("A. Tentang Stunting")
-        st.write("Stunting adalah status kekurangan gizi kronis pada balita yang ditandai dengan Tinggi Badan per Umur (TB/U) < -2 SD pada kurva pertumbuhan WHO. Masa balita termasuk 1000 hari pertama kehidupan adalah masa yang sangat krusial. Keterlambatan tumbuh kembang hingga gagal tumbuh pada masa ini sangat berdampak hingga dewasa. Kondisi ini berdampak negatif pada perkembangan otak dan peningkatan risiko penyakit kronis.")
-        
+        st.write("Stunting adalah status kekurangan gizi kronis pada balita yang ditandai dengan Tinggi Badan per Umur (TB/U) < -2 SD pada kurva pertumbuhan WHO. Masa balita termasuk 1000 hari pertama kehidupan adalah masa yang sangat krusial. Keterlambatan tumbuh kembang hingga gagal tumbuh pada masa ini berdampak negatif pada perkembangan otak, dan meningkatkan risiko berbagai penyakit kronis saat dewasa.")      
     # insert table image. 
         st.image("tbu.jpg")
         st.write("Gambar 1. Tabel standar Panjang atau Tinggi Badan per Umur untuk balita laki-laki. Kategori stunting adalah -2SD hingga -3SD (Pendek), dan <-3SD (Sangat Pendek)")
 
         # Show grafik Penurunan angka stunting di Indonesia tahun 2017-2022/2023
-        tahun = [2016,2017,2019,2021,2022]
-        stunting = [27.5,29.6,27.7,24.4,21.6]
+        tahun = [2016,2017,2018,2019,2021,2022]
+        stunting = [27.5,29.6,30.8,27.7,24.4,21.6]
 
         # Adjust the figure size
         fig, ax = plt.subplots(figsize=(6, 4))  # Set the width and height in inches
@@ -37,7 +37,7 @@ def main():
         ax.plot(tahun, stunting, marker='o')
         ax.set_xlabel('Tahun')
         ax.set_ylabel('Prevalensi Stunting  (%)')
-        ax.set_title('Perubahan Prevalensi Stunting di Indonesia (2017-2022)')
+        ax.set_title('Perubahan Prevalensi Stunting di Indonesia (2016-2022)')
     
         # Set the y-axis range and tick marks
         ax.set_ylim(0, 50)
@@ -51,7 +51,7 @@ def main():
         st.pyplot(fig)
         
         #caption
-        st.write("Gambar 2. Grafik perubahan prevalensi stunting nasional dari tahun 2017-2022.Dari berbagai sumber")
+        st.write("Gambar 2. Grafik perubahan prevalensi stunting nasional dari tahun 2016-2022.Dari berbagai sumber")
 
         # Data stunting semua provinsi di Indonesia tahun 2022 bar chart
         # Data
@@ -119,11 +119,13 @@ def main():
         st.write(" - Relatif mudah didapatkan")
         st.write(" - Pengolahannya lebih cepat dan mudah")
 
+        #pie chart konsumsi telur
+
         st.write("Pada bulan September 2022 lalu, Badan Pusat Statistik (BPS) menerbitkan hasil survey konsumsi telur per kapita per minggu tahun 2022. Jenis telur yang dikonsumsi oleh masyarakat meliputi : Telur ayam ras', 'Telur ayam kampung','Telur itik/telur itik manila','Telur lainnya (telur puyuh, telur asin mentah maupun matang, telur penyu, telur angsa, dsb)'")
         
         #TELUR ANALISA STARTS HERE
         #metrix rata2 konsumsi telur seluruh provinsi as one
-        telur_dataset = "Dataset konsumsi telur perkapita seminggu 2022.xlsx"
+        telur_dataset = "E:/1 TETRIS/project/bertelur/Dataset konsumsi telur perkapita seminggu 2022.xlsx"
         # Read the Excel file with column name in row 3
         df = pd.read_excel(telur_dataset, header=2)
 
@@ -146,13 +148,13 @@ def main():
         columns_to_agg = [col for col in telur_columns if col not in ['Kabupaten/Kota', 'PROVINSI']]
 
         # Group the DataFrame by "PROVINSI" and calculate the minimum, maximum, and average for the selected columns
-        df_telurs_provinsi = df.groupby('PROVINSI')[columns_to_agg].agg(['min', 'max', 'mean'])
+        df_telurs_provinsi = df.groupby('PROVINSI')[columns_to_agg].agg(['mean'])
 
-        # Select the 'mean' level of the MultiIndex columns in df_telurs_provinsi
-        mean_values = df_telurs_provinsi.xs('mean', axis=1, level=1)
+
+        #Sum up all of telurs type
 
         # Sum up the mean values across the telur columns for each province
-        df_telur_provinsi = mean_values.sum(axis=1)
+        df_telur_provinsi = df_telurs_provinsi.sum(axis=1)
 
         # Rename the resulting series column to 'Rata-rata Konsumsi Telur'
         df_telur_provinsi = df_telur_provinsi.rename('Rata-rata Konsumsi Telur')
@@ -180,18 +182,38 @@ def main():
         st.pyplot(fig)
 
         
-        st.write("Gambar 4. Gambaran konsumsi telur per provinsi. Dari 34 provinsi, NTT menjadi provinsi terendah dalam konsumsi telur")
-       # st.write("Dari sini, mari kita buka cerita antara Stunting dan Telurnya")
+        st.write("Gambar 4. Gambaran konsumsi telur per provinsi. Dari grafik di atas dapat diketahui bahwa NTT, Papua, dan Sulawesi Barat menjadi provinsi Zona Merah Stunting yang rata-rata konsumsi telur per kapita < 2 butir per minggu. Sementara Aceh dan NTB sudah berada di angka konsumsi >2 butir.")
         
-              
-         # Load data
-        produksi_telur_dataset = "Data Produksi Telur Ayam Petelur menurut Provinsi 2022.xlsx"
+       #Produksi telur
+          # Load data
+        produksi_telur_dataset = "E:/1 TETRIS/project/bertelur/Data Produksi Telur Ayam Petelur menurut Provinsi 2022.xlsx"
         df_produksi_telur = pd.read_excel(produksi_telur_dataset)
 
-        # Sort df_produksi_telur by province in ascending order
-        df_produksi_telur_sorted = df_produksi_telur.sort_values(by='PROVINSI')
+       
+       # Sort the dataframe by the production column in ascending order
+        df_produksi_telur_sorted = df_produksi_telur.sort_values('2022', ascending=True)
 
-        
+        # Create a vertical bar chart using Plotly Express
+        fig = px.bar(df_produksi_telur_sorted, x='PROVINSI', y='2022', orientation='v', hover_data=['2022'],
+                    labels={'2022': 'Production'}, text='2022')
+
+        # Set the title of the chart
+        fig.update_layout(title='Produksi Telur per Provinsi Tahun 2022')
+
+        # Configure the axis labels
+        fig.update_xaxes(title='Provinsi')
+        fig.update_yaxes(title='Jumlah Produksi Telur (Ton)')
+
+        # Add custom hovertemplate to display value labels
+        fig.update_traces(hovertemplate='<b>%{x}</b><br><br>Production: %{hovertext}')
+
+        # Display the chart using Streamlit
+        st.plotly_chart(fig)
+
+        st.write("Gambar 5. Produksi telur per provinsi pada tahun 2022. Dapat diketahui provinsi-provinsi penghasil telur terbanyak mayoritas ada di pulau Jawa dan Sumatera.")
+
+        st.write("Gambar 5 secara tidak langsung menunjukkan provinsi-provinsi yang menjadi pusat produksi hasil peternakan. Dari data tersebut juga diketahui bahwa provinsi yang berada di Zona Merah Stunting saat ini (NTT, Sulawesi Barat, Papua, NTB, dan Aceh) memproduksi telur ayam sekitar 541-40.000 Ton saja sepanjang tahun 2022. Dibandingkan dengan DI Yogyakarta dan Bali yang memiliki luas wilayah yang kecil dan status prevalensi stunting yang cukup baik, kedua daerah ini memproduksi telur masing-masing sebanyak 168.303 dan 176.855 Ton. Rendahnya produksi telur dalam provinsi dapat membuat harga telur di pasaran setempat lebih tinggi dibanding daerah utama penghasil telur akibat bertambahnya biaya produksi, terutama biaya transportasi. Harga yang semakin mahal dikhawatirkan akan menurunkan kejangkauannya dari masyarakat, sehingga konsumsi telur dan protein berkurang.")
+        #Correlation of produksi and konsumsi telur daerah             
         # Sort df_telur_provinsi by province in ascending order
         df_telur_provinsi_sorted = df_telur_provinsi.sort_values(by='PROVINSI')
 
@@ -208,7 +230,7 @@ def main():
         x = df_produksi_telur_sorted['PROVINSI']
 
         # Plot the bar chart for egg production
-        ax.bar(x, df_produksi_telur_sorted['2022'], width=bar_width, label='Produksi Telur per Bulan (Ribu Ton)')
+        ax.bar(x, df_produksi_telur_sorted['2022'], width=bar_width, label='Produksi Telur per Bulan (Juta Ton)')
 
         # Plot the bar chart for average egg consumption
         ax.bar(x, df_telur_provinsi_sorted['Rata-rata Konsumsi Telur'], width=bar_width, label='Konsumsi Telur per Bulan')
@@ -251,14 +273,14 @@ def main():
         # Display the plot using Streamlit
         st.pyplot(fig)
         
-        st.write("Gambar 5. Gambaran sebaran angka konsumsi telur dihubungkan dengan angka produktivitas telur tiap provinsi")
+        st.write("Gambar 6. Gambaran sebaran angka konsumsi telur dihubungkan dengan angka produktivitas telur tiap provinsi")
 
-        st.write("Plot antara konsumsi telur dan produksi telur daerah di atas menunjukkan bahwa mayoritas penduduk yang mengkonsumsi telur rata-rata 2 butir per minggu berada di provinsi yang produksi telurnya lebih tinggi. Serta, semua penduduk yang mengkonsumsi telur <2 butir per minggu berada di wilayah dengan produksi telur yang rendah. Padahal, jika kita tilik NTT, wilayahnya sangat berpotensi untuk dilakukan pengembangan sektor peternakan.")
+        st.write("Plot antara konsumsi telur dan produksi telur daerah di atas menunjukkan bahwa mayoritas penduduk yang mengkonsumsi telur rata-rata 2 butir per minggu berada di provinsi yang produksi telurnya lebih tinggi. Serta, semua penduduk yang mengkonsumsi telur <2 butir per minggu berada di wilayah dengan produksi telur yang rendah. Hal ini perlu menjadi perhatian pemerintah, untuk meningkatkan produksi telur ayam di Zona Merah Stunting. Dengan adanya peningkatan produksi, diharapkan dapat menekan harga komoditas telur sehingga dapat lebih dijangkau masyarakat.")
         st.subheader("Saran")
-        st.write("Peningkatan produktivitas ternak dan hasil ternak dengan membawa program pemberdayaan masyarakat miskin. Misal, pemerintah dapat mengalihkan bantuan telur dengan pemberdayaan masyarakat melalui membuka usaha ternak ayam yang mana hasilnya dijual ke masyarakat dengan harga jauh lebih murah, serta merekrut pekerja dari warga miskin sekitar.")
-        st.write("Pemberian edukasi kesehatan dan gizi seimbang dari tingkat sekolah hingga Warga masyarakat, khususnya ibu hamil, ibu, calon ibu, calon pengantin, hingga remaja.")
-        st.write("Pemberian pelatihan pertanian dengan memanfaat limbah kotoran ternak sebagai pupuk. Ibu dan calon ibu diberi edukasi pentingnya gizi untuk anak.")
-        st.write("Peningkatan ketersediaan dan pembaruan data yang dapat diakses oleh masyarakat. Termasuk komitmen menyediakan data yang jujur apa adanya. Hal ini berguna untuk meningkatkan peranan swasta dan akademisi dalam menganalisa program dan kinerja guna menangani masalah stunting bersama-sama.")
+        st.write("1. Pemerintah diharapkan dapat melakukan upaya peningkatan produktivitas ternak dan hasil ternak dengan membawa program pemberdayaan masyarakat miskin. Misal, pemerintah dapat mengalihkan bantuan telur dengan pemberdayaan masyarakat melalui usaha ternak ayam yang mana hasilnya dijual ke masyarakat dengan harga jauh lebih murah, serta merekrut pekerja dari warga miskin sekitar.")
+        st.write("2. Peningkatan skill dan pemanfaatan peluang masyarakat melalui pelatihan pertanian dengan memanfaat limbah ternak yang ada. Misal, pemanfaatan kotoran ternak sebagai pupuk.")
+        st.write("3. Peningkatan edukasi dan konseling pada ibu dan calon ibu mengenai pentingnya konsumsi makanan bergizi, salah satunya dengan mengkonsumsi telur. Pemberian edukasi kesehatan dan gizi seimbang dapat dimulai dari tingkat sekolah hingga warga masyarakat, termasuk remaja dan anak-anak.")
+        st.write("4. Peningkatan kualitas dan ketersediaan serta pembaruan data yang dapat diakses oleh masyarakat. Termasuk komitmen menyediakan data yang jujur apa adanya. Hal ini berguna untuk meningkatkan peranan swasta dan akademisi dalam menganalisa program dan kinerja guna menangani masalah stunting bersama-sama.")
 
 
                 
@@ -267,7 +289,6 @@ def main():
         st.header("A. Source and Refference")
         st.write("stunting.go.id")
         st.write("bps.go.id")
-        st.write("stunting.go.id")
         st.write("https://sehatnegeriku.kemkes.go.id/baca/rilis-media/20230125/3142280/prevalensi-stunting-di-indonesia-turun-ke-216-dari-244/")
         st.write("Buku saku hasil Survey Status Gizi Indonesia 2022")
         st.header("B. Tool and Method")
@@ -280,7 +301,7 @@ def main():
         st.write("3. Data Visualisation:")
         st.write("Python: Matplotlib")
         st.write("Streamlit: pyplot ")
-        st.write("Dashboard and Deployment:")
+        st.write("4. Dashboard and Deployment:")
         st.write("Streamlit, GitHub")
     
    
